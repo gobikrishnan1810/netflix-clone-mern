a97268a0086361c98d96e03d27f801dab423a807
gobikrishnan1810 <krishnagobi1810@gmail.com>
2022-03-20 22:05:53 +0530

    ok

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosinstance= axios.create({
-    baseURL:"http://localhost:8080/"
+    baseURL:"https://netflix-mernclone.herokuapp.com/"
 });
 
 export default axiosinstance;
